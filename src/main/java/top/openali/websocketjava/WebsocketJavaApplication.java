f0a271f70c065cd21c387476d5dbfd5403cfb79a
杨强 <yangqiang@bayconnect.com.cn>
2023-06-20 11:24:10 +0800

    feat: 初始化项目

---
+++ src/main/java/top/openali/websocketjava/WebsocketJavaApplication.java
@@ -0,0 +1,13 @@
+package top.openali.websocketjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/openali/websocketjava/WebsocketJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package top.openali.websocketjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
